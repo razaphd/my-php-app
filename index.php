ebf4c2af285d076837bb5119f11a1b7b200fd844
Syed Raza <razaphd@gmail.com>
2018-11-11 14:00:13 -0500

    This is my first PHP commit

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations!</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
+        <h1>Congratulations! This is Awesome!</h1>
+        <p>My AWS Elastic Beanstalk <em>PHP</em> application is now running on my own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
 
